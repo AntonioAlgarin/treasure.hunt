1249fc7647c907effebd6f1b657a4f9a29e2acce
AntonioAlgarin <antonio@cybermindedvets.com>
2022-01-04 16:21:30 -0800

    when I click on one of the question marks an alert appears with the index position of that question mark in the array

--- src/components/Square.js
+++ src/components/Square.js
@@ -2,10 +2,17 @@ import React, { Component } from 'react'
 
 
 class Square extends Component{
+
+  handleClick = () => {
+    alert(this.props.index)
+  }
+
   render(){
     return(
       <>
-        <div className="square">{this.props.value}
+        <div className="square"
+        onClick={this.handleClick}>
+        {this.props.value}
         </div>
       </>
     )
